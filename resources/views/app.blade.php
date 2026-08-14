7a17f7ce73b5e67acc3a0f0864f1378d1fd48174
Geanne Santos <gmaynara@gmail.com>
2026-08-14 20:10:05 -0300

    fix: improve responsive layout for mobile screens

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="pt-BR">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover">
     <title>Batalha de Palavras</title>
     @viteReactRefresh
     @vite(['resources/js/app.tsx', 'resources/css/app.css'])
